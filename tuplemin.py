0ab83d568db85c555a21db2f5b103b4e7b301ec2
77Dinesh <112719731+77Dinesh@users.noreply.github.com>
2022-12-08 02:19:53 +0530

    Update tuplemin.py

--- tuplemin.py
+++ tuplemin.py
@@ -1,10 +1,7 @@
-data = input("data: ")
-list1 = data.split(",")
-
+list1 = input("data: ").split(",")
+#List can be created only this comphrehension.
 for i in range(len(list1)):
     list1[i]= int(list1[i])
-
-
+    
 tup = tuple(list1)
-
 print("min:",min(tup))
